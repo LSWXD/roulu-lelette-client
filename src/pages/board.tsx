d0ca061dfd88a4d43527947320fb227ef213febc
basalcode <basalcode@gmail.com>
2022-01-18 23:51:00 +0900

    Create major pages and directories

---
+++ src/pages/board.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import Layout from '../components/Layout'
+
+const Board: NextPage = () => {
+    return (
+        <Layout>
+            Board
+        </Layout>
+    )
+}
+
+export default Board
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 
+import Layout from '../components/Layout'
+
 const Home: NextPage = () => {
   return (
-    <div>
-      Hello, roulu-lelette!
-    </div>
+    <Layout>
+      Home
+    </Layout>
   )
 }
 
