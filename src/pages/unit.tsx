fa5be14ee0092614313a20993610494b24548631
unknown <woodywoo6049@gmail.com>
2022-01-20 23:24:37 +0900

    Create unit page

---
+++ src/pages/unit.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+import Layout from 'components/common/Layout';
+
+const Unit: NextPage = () => {
+  return (
+    <Layout>
+      Unit Intro
+    </Layout>
+  )
+}
+
+export default Unit
