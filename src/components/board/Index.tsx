9456ebb53db9bb15720788847215794a5a2220a8
basalcode <basalcode@gmail.com>
2022-01-30 23:51:46 +0900

    Create board style

--- src/components/board/Index.tsx
+++ src/components/board/Index.tsx
@@ -1,16 +1,62 @@
+import { useMemo } from 'react';
 import type { NextPage } from 'next';
 
 import Layout from 'components/common/Layout';
 
+import style from './Index.module.scss';
+
 const Index: NextPage = () => {
+    const boardItems = [
+        {
+            userId: "아이디1",
+            ranking: 0,
+            score: 1000
+        },
+        {
+            userId: "아이디2",
+            ranking: 0,
+            score: 100
+        },
+        {
+            userId: "아이디3",
+            ranking: 0,
+            score: 10
+        },
+        {
+            userId: "아이디4",
+            ranking: 0,
+            score: 1
+        }
+    ];
+
+    const items = useMemo(() => {
+        return boardItems.map((boardItem, index) => {
+            return (
+                <div
+                    className={style.item}
+                    key={boardItem.userId}
+                >
+                    <div className={style.userId}>{boardItem.userId}</div>
+                    <div className={style.ranking}>{boardItem.ranking}</div>
+                    <div className={style.score}>{boardItem.score}</div>
+                </div>
+            );
+        });
+    });
+
     return (
         <Layout>
-            <div className="titleContainer">
-                <h1 className="boardTitle"></h1>
-            </div>
-            <div className="boardItemContainer">
-                <div className="boardItem">
-                    
+            <div className={style.boardContainer}>
+                <div className={style.titleContainer}>
+                    <h1 className={style.boardTitle}>스코어 보드</h1>
+                </div>
+                <div className={style.boardItems}>
+                    <div className={style.item}>
+                        <div className={style.userId}>유저 아이디</div>
+                        <div className={style.ranking}>랭킹</div>
+                        <div className={style.score}>점수</div>
+                    </div>
+                    {items}
                 </div>
             </div>
         </Layout>
