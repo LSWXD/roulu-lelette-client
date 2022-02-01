5898c03611b212c87c31b0e827ea19c4f6618cb3
basalcode <basalcode@gmail.com>
2022-02-01 23:47:02 +0900

    Create slot items

--- src/components/game/Index.tsx
+++ src/components/game/Index.tsx
@@ -3,20 +3,57 @@ import type { NextPage } from 'next';
 import Layout from 'components/common/Layout';
 
 import style from './Index.module.scss';
+import { useMemo } from 'react';
 
 const Index: NextPage = () => {
+    const slot1 = useMemo(() => {
+        const minRotation: number = 5;
+        const maxRotation: number = 10;
+        const rotation: number = Math.floor(Math.random() * maxRotation);
+        const rotator = new Array(rotation).fill(true);
+        const slotItems = [
+            {
+                name: "시공 좋아",
+            },
+            {
+                name: "Yee"
+            },
+            {
+                name: "Pop cat"
+            },
+            {
+                name: "이차는 제껍니다"
+            }
+        ];
+
+        return rotator.map((_) => {
+            return slotItems.map((slotItem, index) => {
+                const name = slotItem.name;
+                return (
+                    <div
+                        key={name}
+                        className={style.slotItem}
+                    >
+                        {name}
+                    </div>
+                )
+            });
+        });
+    }, []);
+
+
     return (
         <Layout>
             <div className={style.rouletteContainer}>
                 <div className={style.roulette}>
                     <div className={style.rouletteSlot}>
-                        슬롯 1
+                        {slot1}
                     </div>
                     <div className={style.rouletteSlot}>
-                        슬롯 2
+                        {slot1}
                     </div>
                     <div className={style.rouletteSlot}>
-                        슬롯 3
+                        {slot1}
                     </div>
                 </div>
                 <div className={style.buttonContainer}>
