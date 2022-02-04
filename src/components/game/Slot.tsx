ce019e5914d5cca7f59a1714dea1969bdd736f1c
basalcode <basalcode@gmail.com>
2022-02-04 23:55:00 +0900

    Create slot animation

--- src/components/game/Slot.tsx
+++ src/components/game/Slot.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next';
 
 import style from './Slot.module.scss';
 import { useEffect, useMemo, useRef, useState } from 'react';
+import { clear } from 'console';
 
 const Slot: NextPage = () => {
     const scrollRef = useRef(null);
     const [scrollElement, setScrollElement] = useState(null);
+    const [scrollMaxHeight, setScrollMaxHeight] = useState(-1);
     const [scrollTop, setScrollTop] = useState<number>(-1);
-
+    const [scrollInterval, setScrollInterval] = useState(-1);
     const slot = useMemo(() => {
         const minRotation: number = 5;
         const maxRotation: number = 10;
@@ -60,19 +62,38 @@ const Slot: NextPage = () => {
 
         scrollElement.scrollTo(0, scrollHeight);
 
+        const second = 1000;
+        const minimumSecond = 3 * second;
+        const maximumSecond = 5 * second;
+        const slotEndTimer = (minimumSecond + Math.floor(Math.random() * maximumSecond)) % maximumSecond;
+        const timeInterval = 10;
+        const initialScrollInterval = timeInterval / slotEndTimer * scrollElement.scrollTop;
+
+        console.log("slotEndTimer", slotEndTimer)
+
+
         setScrollElement(scrollElement);
-        setScrollTop(scrollHeight);
+        setScrollTop(scrollElement.scrollTop);
+        setScrollMaxHeight(scrollElement.scrollTop);
+        setScrollInterval(initialScrollInterval);
     }, [scrollRef]);
 
-    // useEffect(() => {
-    //     if (!scrollElement) return;
-    //     if (scrollTop === -1) return;
+    useEffect(() => {
+        if (!scrollElement) return;
+        if (scrollTop === -1) return;
+        if (scrollInterval === -1) return;
 
-    //     const interval = 1000;
-    //     scrollElement.scrollTo(0, scrollTop - 100);
+        const timeInterval = 10;
 
-    //     // setTimeout(() => setScrollTop(scrollTop - 5), interval);
-    // }, [scrollTop]);
+        const intervalScrollTop = scrollTop - scrollInterval;
+        const newScrollTop = intervalScrollTop < 0 ? 0 : intervalScrollTop;
+
+        scrollElement.scrollTo(0, newScrollTop);
+
+        const timeoutId = setTimeout(() => setScrollTop(newScrollTop), timeInterval);
+
+        return () => clearTimeout(timeoutId);
+    }, [scrollTop]);
 
     return (
         <div
